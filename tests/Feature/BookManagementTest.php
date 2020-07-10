52cbd3b0c9020151502263d2069a3599d2d5ee7b
Back1ng <anatolyu128@gmail.com>
2020-07-10 21:27:16 +0500

    works with books

--- tests/Feature/BookManagementTest.php
+++ tests/Feature/BookManagementTest.php
@@ -6,22 +6,22 @@ use App\Book;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class BookReservationTest extends TestCase
+class BookManagementTest extends TestCase
 {
     use RefreshDatabase;
 
     /** @test  */
     public function testABookCanBeAddedToTheLibrary()
     {
-        $this->withoutExceptionHandling();
-
         $response = $this->post('/books', [
             'title' => "Cool Book Title",
             'author' => "Victor",
         ]);
 
-        $response->assertOk();
         $this->assertCount(1, Book::all());
+        $book = Book::first();
+
+        $response->assertRedirect($book->path());
     }
 
     /** @test */
@@ -49,9 +49,6 @@ class BookReservationTest extends TestCase
     /** @test */
     public function testABookCanBeUpdated()
     {
-        $this->withoutExceptionHandling();
-
-
         $this->post('/books', [
             'title' => 'Cool Title',
             'author' => 'Victor'
@@ -59,12 +56,29 @@ class BookReservationTest extends TestCase
 
         $book = Book::first();
 
-        $response = $this->patch('/books/' . $book->id, [
+        $response = $this->patch($book->path(), [
             'title' => 'New Title',
             'author' => 'New Author'
         ]);
 
         $this->assertEquals('New Title', Book::first()->title);
         $this->assertEquals('New Author', Book::first()->author);
+        $response->assertRedirect($book->fresh()->path());
+    }
+
+    public function testABookCanBeDeleted()
+    {
+        $this->post('/books', [
+            'title' => 'Cool Title',
+            'author' => 'Victor'
+        ]);
+
+        $book = Book::first();
+        $this->assertCount(1, Book::all());
+
+        $response = $this->delete($book->path());
+
+        $this->assertCount(0, Book::all());
+        $response->assertRedirect("/books");
     }
 }
